36ff9f376b79ac640093071c0c43c3f01509fb2d
Ruman Gerst <ruman.gerst@uni-jena.de>
2016-10-12 01:37:12 +0200

    * now version 1.0.1.1

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     }
 
     QApplication::setApplicationName("SilVerPLuM");
-    QApplication::setApplicationVersion("1.0.1.0");
+    QApplication::setApplicationVersion("1.0.1.1");
 
     MainWindow w;
     w.show();
