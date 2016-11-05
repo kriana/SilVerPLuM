dbc2db3708d65f129aef320716084df9e3f5f4dc
Ruman Gerst <ruman.gerst@uni-jena.de>
2016-11-05 15:12:32 +0100

    * More readable messages

--- src/messagewidget.cpp
+++ src/messagewidget.cpp
@@ -13,6 +13,17 @@ MessageWidget::MessageWidget(QWidget *parent) :
     hide();
 
     ui->btnAction->hide();
+
+    // Build style
+    QColor background_top = palette().color(QPalette::Active, QPalette::Highlight);
+    QColor background_bottom = background_top.dark(120);
+    QColor border = palette().color(QPalette::Active, QPalette::Shadow);
+
+    ui->frame->setStyleSheet(QString("QFrame#frame {"
+                                    "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 %1, stop:1 %2);"
+                                    "border: 1px solid %3;"
+                                    "border-radius: 3px;"
+                                    "}").arg(background_top.name()).arg(background_bottom.name()).arg(border.name()));
 }
 
 MessageWidget::~MessageWidget()
